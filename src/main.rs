75b6e0ca8b0b11ff1db90d1274c762cb323de588
Sathish Kannan <sathishkannan162@gmail.com>
2024-01-06 01:29:25 +0530

    Completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let hello = "hello world from pigs apple for us";
+    let mut pig_latin = String::new();
+
+    for word in hello.split_whitespace() {
+        let first_letter = word.chars().nth(0).expect("There is no word");
+        match first_letter {
+            'a' | 'e' | 'i' | 'o' | 'u' => {
+                let n = word.to_owned() + "-hay ";
+                pig_latin.push_str(&n);
+            }
+            _ => {
+                let m = &word[1..];
+                let n = m.to_owned() + "-" + &String::from(first_letter) + "ay ";
+                pig_latin.push_str(&n);
+            }
+        };
+    }
+    println!("{pig_latin}");
 }
